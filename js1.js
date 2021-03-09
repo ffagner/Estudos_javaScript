4ee64d3d6fa6e4a32bbe4d071e4f9f7020b52809
ffagner <farias.live@gmail.com>
2021-03-08 22:27:33 -0300

    testes

---
+++ js1.js
@@ -0,0 +1,8 @@
+// imprimindo mensagens ao usuário
+// console.log("fagner martins farias");
+// alert("asdasad")
+let texto1 = "fagner";
+let texto2 = "martins";
+let texto3 = "farias";
+
+console.log(`seu nome é ${texto1} o seu sobrenome e ${texto2} e seu ultimo nome e ${texto3}`);
