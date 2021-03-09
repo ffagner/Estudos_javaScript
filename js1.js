036b68ab6599060a74afd3a4f52b90a2b3396761
ffagner <farias.live@gmail.com>
2021-03-09 10:28:06 -0300

    operadores aritiméticos

--- js1.js
+++ js1.js
@@ -10,3 +10,14 @@ console.log(`seu nome é ${texto1} o seu sobrenome e ${texto2} e seu ultimo nome
 // notas
 // tipos de dados primitivos
 // String, Number, null, undefined, Symbol e Boolean
+
+// Operadores aritiméticos
+/**
+ * + adição e concatenação
+ * - subtração
+ *  / divisão
+ *  * multiplicação
+ *  ** potenciação
+ *  % resto da divisão 
+ *  
+ */
